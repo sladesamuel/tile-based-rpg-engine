565c47c288930cd334aa63d97f071486312518a4
Samuel Slade <samuel.slade@sladesoftware.co.uk>
2021-10-09 17:47:05 +0100

    Added Tiled project with Home map

---
+++ SandboxRpg/Content/Maps/Home.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="Home" tilewidth="16" tileheight="16" tilecount="16384" columns="128">
+ <image source="Home.png" width="2048" height="2048"/>
+</tileset>
